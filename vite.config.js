17bcd6511cb4a4add4fa2e448818c0d805b47d57
Mayank Verma <mayankverma.dev@gmail.com>
2025-02-22 14:58:03 +0530

    added base url-name in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://wannacryy24.github.io/GitActions/',
   plugins: [react()],
 })
