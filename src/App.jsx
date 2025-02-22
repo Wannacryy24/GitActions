a962ad957514619d1434a6389a13101afc075017
Mayank Verma <mayankverma.dev@gmail.com>
2025-02-22 15:06:50 +0530

    added base url-name in vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import One from './One'
-import Two from './Two'
-
 function App() {
-  const [message, setMessage] = useState('')
-
   return (
     <>
-      {message === 'one' && <One />}
-      {message === 'two' && <Two />}
-      <button onClick={() => setMessage('one')}>One</button>
-      <button onClick={() => setMessage('two')}>Two</button>
+      <h1>Welocme to ci/cd Git Actions</h1>
     </>
   )
 }
